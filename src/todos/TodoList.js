6c5f02d5110bbc294490d1f9d5b606e2e6492eda
Davis Okioma <kuntaytc@gmail.com>
2021-10-07 00:15:34 +0300

    added mark as complete feature

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -5,15 +5,31 @@ import TodoListItem from './TodoListItem';
 import { removeTodo, markTodoAsCompleted } from './actions';
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
-    <div className="list-wrapper">
-        <NewTodoForm />
-        {todos.map(todo => <TodoListItem
-            todo={todo}
-            onRemovePressed={onRemovePressed}
-            onCompletedPressed={onCompletedPressed}/>)}
-    </div>
-);
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => {
+	const completedTodos = todos.filter(todo => todo.isCompleted)
+	const notCompletedTodos = todos.filter(todo => !todo.isCompleted)
+
+
+	return (	
+		<div className="list-wrapper">
+			<NewTodoForm />
+			<h2>{notCompletedTodos.length | completedTodos.length ? null : "No todos to display 😥"}</h2>
+			<h2>{notCompletedTodos.length ? "Pending Todos" : null}</h2>
+			{notCompletedTodos.map(todo => <TodoListItem
+				todo={todo}
+				onRemovePressed={onRemovePressed}
+				onCompletedPressed={onCompletedPressed}/>
+				)
+			}
+			<h2>{completedTodos.length ? "Completed Todos" : null}</h2>
+			{completedTodos.map(todo => <TodoListItem
+				todo={todo}
+				onRemovePressed={onRemovePressed}/>
+				)
+			}
+		</div>
+	)
+};
 
 const mapStateToProps = state => ({
     todos: state.todos,
